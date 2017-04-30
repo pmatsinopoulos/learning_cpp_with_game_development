d409e831ca07666c52e6fdb3cf1096852f3457ce
Panayotis Matsinopoulos <panayotis@matsinopoulos.gr>
2017-04-30 09:02:07 +0300

    Using do while loop instead of while loop

--- play_again/play_again.cpp
+++ play_again/play_again.cpp
@@ -2,12 +2,13 @@
 using namespace std;
 
 int main () {
-  char answer = 'y';
-  while (answer == 'y') {
+  char answer;
+  do {
     cout << "** Played an exciting game ** " << endl;
     cout << " Do you want to play again ? (y/n) " << endl;
     cin >> answer;
-  }
+  } while (answer == 'y');
+
   return 0;
 }
 
